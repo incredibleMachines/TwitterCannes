b525bdc3419180b3618e7dfea29aa351926ec847
Scott Peterman <scott@incrediblemachines.com>
2013-05-24 11:06:55 -0400

    Variation of 6 with the fluttering in place of tiles after coming together

---
+++ Prototype_MF_7/src/particle.cpp
@@ -0,0 +1,148 @@
+//
+//  particle.cpp
+//  scott_particles
+//
+//  Created by IncredibleMachines on 5/20/13.
+//
+//
+
+#include "particle.h"
+
+#define tileW 20
+#define tileH 20
+
+void Particle::setup(ofPoint pixel, ofImage image, float imageWidth, float imageHeight, float i, float total) {
+
+	this->image = image;
+	imageW = imageWidth;
+	imageH = imageHeight;
+	
+	pixelPos.x = pixel.x;
+	pixelPos.y = pixel.y;
+	
+	target.x = pixelPos.x + ((ofGetWidth() - imageW) / 2);
+	target.y = pixelPos.y + ((ofGetHeight() - imageH) / 2);
+	target.z = 0;
+	
+	targetReached = false;
+	
+	// store which tile this is relative to others, so we can make some
+	// come in later than others, for example
+	myIndex = i / total;
+	
+	// for diversity in starting positions
+	offset = ofRandom(30, 50);
+    timeOffset = ofRandom(0, 1);
+	
+	// Generate a random rotation type 0..2 which will determine
+	// different values for axes of rotation
+	rotationType = ofRandom(8);
+	
+}
+
+void Particle::update(){
+    
+    if (myIndex == 1) {
+        cout << targetReached << endl;
+    }
+    
+    if (!targetReached) {
+    
+    //	float time = MIN(ofGetElapsedTimef(), PI*3/2);
+        float time = ofGetElapsedTimef();
+        float t = ofMap(sin(time+PI/4-timeOffset), -1, 1, 0, 50);
+    //	float t = 0;
+        
+        pos.x = target.x + sin(t/2 + offset) * 4 * t;
+        pos.y = target.y + cos(t/2 + offset * 2) * 5 * t;
+        pos.z = target.z + powf(t, 1.75) * offset / 40;
+
+        
+        // Reached destination
+        // Takes a few seconds for this condition to become true after
+        // image appears complete
+        
+        float xDiff = abs(pos.x - target.x);
+        float yDiff = abs(pos.y - target.y);
+        
+        if (xDiff < 0.01 && yDiff < 0.01) {
+            targetReached = true;
+        }
+        
+        prevPos = pos;
+        
+    }
+}
+
+void Particle::draw(){
+    
+//	float time = MIN(ofGetElapsedTimef(), PI*3/2);
+	float time = ofGetElapsedTimef();
+	float t = ofMap(sin(time+PI/4-timeOffset), -1, 1, 0, 25);
+//	float t = 0;
+
+	float rotateX, rotateY, rotateZ;
+	
+	switch (rotationType) {
+		case 0:
+			rotateX = offset * t / 50 * RAD_TO_DEG;
+			rotateY = offset * t / 40 * RAD_TO_DEG;
+			rotateZ = offset * t / 30 * RAD_TO_DEG;
+			break;
+			
+		case 1:
+			rotateX = offset * t / 50 * RAD_TO_DEG * -1;
+			rotateY = offset * t / 40 * RAD_TO_DEG;
+			rotateZ = offset * t / 30 * RAD_TO_DEG;
+			break;
+			
+		case 2:
+			rotateX = offset * t / 50 * RAD_TO_DEG;
+			rotateY = offset * t / 40 * RAD_TO_DEG * -1;
+			rotateZ = offset * t / 30 * RAD_TO_DEG;
+			break;
+			
+		case 3:
+			rotateX = offset * t / 50 * RAD_TO_DEG * -1;
+			rotateY = offset * t / 40 * RAD_TO_DEG * -1;
+			rotateZ = offset * t / 30 * RAD_TO_DEG;
+			break;
+			
+		case 4:
+			rotateX = offset * t / 50 * RAD_TO_DEG;
+			rotateY = offset * t / 40 * RAD_TO_DEG;
+			rotateZ = offset * t / 30 * RAD_TO_DEG * -1;
+			break;
+			
+		case 5:
+			rotateX = offset * t / 50 * RAD_TO_DEG * -1;
+			rotateY = offset * t / 40 * RAD_TO_DEG;
+			rotateZ = offset * t / 30 * RAD_TO_DEG * -1;
+			break;
+			
+		case 6:
+			rotateX = offset * t / 50 * RAD_TO_DEG;
+			rotateY = offset * t / 40 * RAD_TO_DEG * -1;
+			rotateZ = offset * t / 30 * RAD_TO_DEG * -1;
+			break;
+			
+		case 7:
+			rotateX = offset * t / 50 * RAD_TO_DEG * -1;
+			rotateY = offset * t / 40 * RAD_TO_DEG * -1;
+			rotateZ = offset * t / 30 * RAD_TO_DEG * -1;
+			break;
+	}
+	
+	ofPushMatrix();
+	ofTranslate(pos);
+    ofRotateX(rotateX);
+    ofRotateY(rotateY);
+    ofRotateZ(rotateZ);
+	image.draw(0, 0, tileW, tileH);
+	ofPopMatrix();
+	//ofCircle(pos, 2);
+}
+
+void Particle::flipTarget() {
+	
+}
---
+++ Prototype_MF_7/src/testApp.cpp
@@ -0,0 +1,148 @@
+#include "testApp.h"
+
+#define tileW 20
+#define tileH 20
+
+#define USE_DOF true
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    ofSetVerticalSync(true);
+	ofSetFrameRate(60);
+	//ofEnableSmoothing();
+	ofSetRectMode(OF_RECTMODE_CENTER);
+    
+	//ofSetCircleResolution(10);
+    
+    pic.loadImage("image.jpg");
+    pic.setImageType(OF_IMAGE_COLOR);
+	pic.resize(pic.width/2, pic.height/2);
+	pic.mirror(true, true);
+    
+    populatePixels();
+    //createParticles();
+	
+	ofBackground(255);
+	ofSetColor(255);
+	
+	
+	if (USE_DOF) {
+		dof.setup(ofGetWidth(), ofGetHeight());
+		dof.setFocalDistance(347);
+		dof.setFocalRange(15);
+        dof.setBlurAmount(1);
+	}
+
+//
+//	camera.setDistance(1000);
+//	camera.setFov(100);
+	
+
+
+//	camera.rotateAround(90, ofPoint(0, 0, 1), camera.getPosition());
+//	camera.roll(30);
+
+	camera.setDistance(100);
+	camera.setPosition(ofGetWidth()/2, ofGetHeight()/2, 1000);
+	camera.lookAt(ofPoint(ofGetWidth()/2, ofGetHeight()/2, 10));
+    
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	bool flipTarget = true;
+	
+    for(int i=0; i<particles.size(); i++){
+        particles[i].update();
+		
+		if (!particles[i].targetReached) {
+			flipTarget = false;
+		}
+    }
+	
+	if (flipTarget) {
+        cout << "Flipping" << endl;
+		for(int i=0; i<particles.size(); i++){
+			particles[i].flipTarget();
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	if (USE_DOF) {
+		dof.begin();
+		camera.begin(dof.getDimensions());
+	} else {
+		camera.begin();
+	}
+
+	
+	
+	ofSetColor(255);
+//	ofRect(0, 0, ofGetWidth(), ofGetHeight());
+	
+    for(int i=0; i<particles.size(); i++){
+        particles[i].draw();
+    }
+	
+	camera.end();
+	if (USE_DOF) {
+		dof.end();
+        ofPushMatrix();
+		
+        ofTranslate(ofGetWidth()/2,ofGetHeight()/2);
+        ofRotate(180);
+        dof.getFbo().draw(0,0,ofGetWidth(),ofGetHeight());
+
+        ofPopMatrix();
+	}
+	
+	
+}
+
+void testApp::populatePixels(){
+    //imagePixels = pic.getPixels();
+	
+	int counter = 0;
+	int total = pic.width/tileW * pic.height/tileH;
+	
+    for (int x = 0; x < pic.width / tileW; x++){
+		for (int y = 0; y < pic.height / tileH; y++){
+			
+			counter++;
+			
+			ofImage tileImage;
+			//tileImage.clone(pic);
+			//tileImage.crop(x*tileW, y*tileH, tileW, tileH);
+			tileImage.allocate(tileW, tileH, OF_IMAGE_COLOR);
+			tileImage.cropFrom(pic, x*tileW, y*tileH, tileW, tileH);
+			
+			Particle p;
+            p.setup(ofPoint(x*tileW, y*tileH), tileImage, pic.width, pic.height, counter, total);
+			particles.push_back(p);
+		}
+	}
+}
+
+void testApp::createParticles(){
+//    for(int i=0;i<pixels.size();i++){
+//        Particle newParticle;
+//        newParticle.setup(pixels[i].pos, pixels[i].r, pixels[i].g, pixels[i].b, pic.getPixels(), pic.width, pic.height);
+//        particles.push_back(newParticle);
+//    }
+}
+
+void testApp::mouseMoved(int x, int y) {
+
+//	dof.setFocalRange(ofMap(x, 0, ofGetWidth(), -1000, 1000));
+//	cout << dof.getFocalRange() << endl;
+	
+}
+
+void testApp::mouseDragged(int x, int y, int button) {
+	printf("Position: %f, %f, %f\n", camera.getX(), camera.getY(), camera.getZ());
+	printf("Target: %f, %f, %f\n", camera.getTarget().getX(), camera.getTarget().getY(), camera.getTarget().getZ());
+	printf("Roll: %f\n", camera.getRoll());
+	printf("Distance: %f\n", camera.getDistance());
+}
