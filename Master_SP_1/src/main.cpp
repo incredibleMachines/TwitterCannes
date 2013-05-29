d8d8e17a2005ef0d205d0fd7b67d0fda5337ae2d
Matt Felsen <mattfelsen@gmail.com>
2013-05-29 16:40:30 -0400

    Added line for anti-aliasing though it's off for now

--- Master_SP_1/src/main.cpp
+++ Master_SP_1/src/main.cpp
@@ -5,6 +5,10 @@
 int main(){
 	ofAppGlutWindow window; // create a window
 	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
+    
+    // uncomment this line for antialiasing
+    //window.setGlutDisplayString("rgba double samples>=4");
+    
 	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
 	ofRunApp(new testApp()); // start the app
 }
