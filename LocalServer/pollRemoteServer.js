37023d32488e4341c1f9019dc4d9ca819445b557
Matt Felsen <mattfelsen@gmail.com>
2013-06-13 18:34:40 -0400

    Add image downloading code & modify path from a full URL to a local filesystem path

--- LocalServer/pollRemoteServer.js
+++ LocalServer/pollRemoteServer.js
@@ -1,34 +1,36 @@
 var http = require('http');
+var fs = require('fs');
+var request = require('request');
 
 var MongoClient = require('mongodb').MongoClient
 	,Server = require('mongodb').Server
 	,mongo = new MongoClient(new Server('localhost', 27017))
 	,BSON = require('mongodb').BSONPure;
-	
+
 var db, collection;
 
 mongo.open(function(err,mongo){
-	
+
 	db = mongo.db('twitterCannesLions');
 	collection = db.collection('tweets');
-	
+
 });
 
 
-setInterval(checkRemote, 1000*60); //one minute
+setInterval(checkRemote, 1000*10); //one minute
 
 function checkRemote(){
 	var body=''; 
 	var json;
 	var timestamp;
-	
+
 	collection.find().sort({"approved_at":-1}).limit(1).toArray( function(err,doc){
-		
+
 		if(!doc.length){
 			//console.log('Empty');
-			timestamp = '2013-06-12+00:00:00';
+			timestamp = '2012-06-12+00:00:00';
 			console.log(timestamp);
-			
+
 		}else{
 			var newtime = doc[0].approved_at;
 			timestamp=newtime.replace(' ','+');
@@ -39,48 +41,48 @@ function checkRemote(){
 			host: 'ec2-23-23-188-178.compute-1.amazonaws.com',
 			port: 9000,
 			path: '/?since='+timestamp+'&limit=0'
-	
+
 		};
 
 		http.get(options,function(res){
-			
+
 			console.log(res.statusCode);
-			
+
 			res.on('data',function(chunk){
-				
+
 				body+= chunk;
-		
+
 			});
 			res.on('end',function(){
-				
+
 				//console.log(body);
 				json = JSON.parse(body);
-	
+
 				for(var i = 0; i< json.length; i++){
 						insertOrUpdate(json[i]);
 				}
-				
+
 			});
-			
+
 		}).on('error',function(e){
-			
+
 			console.log('Get Error: '+e);
 		});
-	
-		
+
+
 	});	
-	
+
 }
 
 
 function insertOrUpdate(json){
-	
-	
+
+
 	var oID = {"_id": new BSON.ObjectID(json._id)};
 	collection.findOne(oID, function(err,result){
-		
+
 		if(!result){
-			
+
 			var obj = {
 				"_id": new BSON.ObjectID(json._id),
 				"approved_at": json.approved_at, //
@@ -98,26 +100,41 @@ function insertOrUpdate(json){
 				"shown_count": 0,
 				"last_shown": "",
 				"id":json.id
-				
+
 			};
-				
+
+			if (obj.user_image != "") {
+				var user_image_path = "/twitter/user_images/" + obj.user_id + "." + obj.user_image.split('.').reverse()[0];
+				fetchImage(obj.user_image, user_image_path);
+				obj.user_image = user_image_path;
+			}
+			if (obj.media_url != "") {
+				var media_url_path = "/twitter/media_images/" + obj.id + "." + obj.media_url.split('.').reverse()[0];
+				fetchImage(obj.media_url, media_url_path);
+				obj.media_url = media_url_path;
+			}
+
 			collection.insert(obj, {safe:true},function(err,doc){
 				if(err)throw err;
-				
+
 			});
-			
+
 		}else{
-			
+
 			var oID = {"_id": new BSON.ObjectID(json._id) };
 			var obj = { $set: { "approved_at": json.approved_at, "category": json.category, "starred": json.starred}};
-			
+
 			collection.update(oID, obj, function(err){
 				if(err) throw err;
 			});
 		}
-	
+
 	});
-				
-	
-	
+
 }
+
+var fetchImage = function(uri, outfile) {
+	request.head(uri, function(err, res, body) {
+		request(uri).pipe(fs.createWriteStream(outfile));
+	});
+};
